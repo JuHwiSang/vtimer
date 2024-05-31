39d61bb4ae067b86e096c6105251699ca8d05cb7
JuHwiSang <juhs2005@gmail.com>
2024-05-31 17:35:30 +0900

    feat: new internal utility CallStackObserver

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,37 @@ let idNum = 1;
 export function uniqueId() {
     return idNum++;
 }
+
+export function setPop<T>(set: Set<T>) : T | undefined {
+    const val = set.values().next().value;
+    set.delete(val);
+    return val;
+}
+
+export class CallStackObserver {
+    protected immediate?: NodeJS.Immediate
+    public onCallStackEmpty?: () => any
+
+    constructor() {}
+
+    isOn() {
+        return this.immediate !== undefined;
+    }
+
+    on() {
+        if (this.immediate === undefined) {
+            this.immediate = setImmediate(() => {
+                if (this.onCallStackEmpty) {
+                    this.off();
+                    this.on();
+                    this.onCallStackEmpty();
+                }
+            });
+        }
+    }
+
+    off() {
+        clearImmediate(this.immediate);
+        delete this.immediate;
+    }
+}
